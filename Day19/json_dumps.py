28951ae03471e74c7aad18832c2d1a5addc9e64e
Sowndarya128 <sowndaryaraj128@gmail.com>
2026-06-11 17:57:37 +0530

    added day19 python programs

---
+++ Day19/json_dumps.py
@@ -0,0 +1,7 @@
+import json
+student = {
+    "name": "Sowndaraya",
+    "age": "22"
+}
+data = json.dumps(student)
+print(data)
---
+++ Day19/json_loads.py
@@ -0,0 +1,4 @@
+import json
+data = '{ "name": "Sowndaraya","age": "22"}'
+student = json.loads(data)
+print(student)
