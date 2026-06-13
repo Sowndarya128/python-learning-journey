811bf47f575905b934d6e251d5084f6f9a0046ba
Sowndarya128 <sowndaryaraj128@gmail.com>
2026-06-13 17:02:03 +0530

    added day21 python programmes

---
+++ Day 21/inheritance.py
@@ -0,0 +1,7 @@
+class Animal: 
+    def sound(self):
+        print("Animal sound")
+class Dog(Animal):
+    pass
+d = Dog()
+d.sound()
---
+++ Day 21/polymorphism.py
@@ -0,0 +1,10 @@
+class Dog:
+    def sound(self):
+        print("Bark")
+class Cat:
+    def sound(self):
+        print("Meow")
+d = Dog()
+c = Cat()
+d.sound()
+c.sound()
