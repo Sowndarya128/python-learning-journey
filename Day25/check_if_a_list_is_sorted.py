f08d7c06e2bfdde04581675e2eaca213375a4c32
Sowndarya128 <sowndaryaraj128@gmail.com>
2026-06-20 17:00:11 +0530

    added day25 python programs

---
+++ Day25/check_if_a_list_is_sorted.py
@@ -0,0 +1,5 @@
+list = [10,20,30,40]
+if list == sorted(list):
+    print("Sorted")
+else:
+    print("Not sorted")
---
+++ Day25/missing_numbers.py
@@ -0,0 +1,4 @@
+numbers = [1,2,4,5]
+for n in range(1,6):
+    if n not in numbers:
+        print(n)
